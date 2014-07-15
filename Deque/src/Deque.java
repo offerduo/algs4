31f510c7cd938a343f6d0dd293a826b972d48686
offerduo <offerduo@gmail.com>
2014-07-15 12:26:24 -0700

    Add code sketch for Deque class

--- Deque/src/Deque.java
+++ Deque/src/Deque.java
@@ -1,4 +1,40 @@
+import java.util.Iterator;
 
-public class Deque {
+public class Deque<T> implements Iterable<T> {
+  private class DoublyLinkedNode {
+    T                item;
+    DoublyLinkedNode prev;
+    DoublyLinkedNode next;
+  }
+
+  private DoublyLinkedNode first, last;
+
+  private class ListIterator implements Iterator<T> {
+
+    @Override
+    public boolean hasNext() {
+      // TODO Auto-generated method stub
+      return false;
+    }
+
+    @Override
+    public T next() {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
+    @Override
+    public void remove() {
+      // TODO Auto-generated method stub
+
+    }
+
+  }
+
+  @Override
+  public Iterator<T> iterator() {
+    // TODO Auto-generated method stub
+    return new ListIterator();
+  }
 
 }
